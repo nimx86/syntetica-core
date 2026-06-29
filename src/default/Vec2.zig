14dc2ff3834795beefb0e476d61b4c0dca4ef23f
Vasilix <lvasilicc@proton.me>
2026-06-29 18:32:47 +0200

    vector2 documentation

--- src/default/Vec2.zig
+++ src/default/Vec2.zig
@@ -21,25 +21,32 @@ pub fn PhVec(T: type) type {
     };
 }
 
+/// create a vector type with the fields of it being the value of argument `T`
 pub fn Vec2(T: type) type {
     return struct {
+        /// coordinate across the x-axis
         x: T,
+        /// coordinate across the y-axis
         y: T,
 
         const Self = @This();
 
+        /// create a vector
         pub fn init(x: T, y: T) Self {
             return .{.x = x, .y = y};
         }
 
+        /// initializes both fields of the vector with a scalar value
         pub fn initScalar(v: T) Self {
             return .{.x = v, .y = v};
         }
 
+        /// depreciated, use .init(...)
         pub fn val(x: T, y: T) Self {
             return .{.x = x, .y = y};
         }
 
+        /// calculates the distance between two points
         pub fn dist(self: Self, b: @TypeOf(Self)) f64 {
             const A = @Vector(2, T){self.x, self.y};
             const B = @Vector(2, T){b.x, b.y};
@@ -50,26 +57,31 @@ pub fn Vec2(T: type) type {
             return @sqrt(part1 + part2);
         }
 
+        /// adds two vectors together
         pub fn add(self: *Self, b: Self) void {
             self.x += b.x; 
             self.y += b.y;
         }
 
+        /// adds a scalar value to both fields of a vector
         pub fn addScalar(self: *Self, v: T) void {
             self.x += v;
             self.y += v;
         }
 
+        /// subtracts two vectors
         pub fn sub(self: *Self, b: Self) void {
             self.x -= b.x;
             self.y -= b.y;
         }
 
+        /// subtracts a scalar value from both fields of a vector
         pub fn subScalar(self: *Self, v: T) void {
             self.x -= v;
             self.y -= v;
         }
 
+        /// depreciated
         pub fn toPhVec(self: Self) PhVec(T) {
             const magnitute: T = @sqrt(self.x * self.x + self.y * self.y);
             
@@ -78,6 +90,7 @@ pub fn Vec2(T: type) type {
             return .{.magnitute = magnitute, .direction = dir};
         }
 
+        /// adds a value to the vector's magnitude
         pub fn addMagnitude(self: *Self, f: T) void {
             // if we have a 0 vector, we don't know in which 
             // direction to add the magnitude.
@@ -100,10 +113,12 @@ pub fn Vec2(T: type) type {
             self.* = V;
         } 
 
+        /// returns the magnitude of a vector
         pub fn getMagnitude(self: *Self) T {
             return @sqrt(self.x * self.x + self.y * self.y);
         }
 
+        /// sets the magnitude of a vector to a specific value
         pub fn setMagnitude(self: *Self, m: T) void {
             if(self.x == 0 and self.y == 0) return;
 
@@ -117,15 +132,18 @@ pub fn Vec2(T: type) type {
             self.* = V;
         }
 
+        /// subtracts a scalar value from the magnitude of a vector
         pub fn subMagnitude(self: *Self, f: T) void {
             self.addMagnitude(-f);
         }
 
+        /// clamps the magnitude of a vector to a specific value
         pub fn clamp(self: *Self, v: T) void {
             if(self.getMagnitude() < v) return;
             self.setMagnitude(v);
         }
 
+        /// rotates a vector by an angle speicifed in radians
         pub fn rotate(self: *Self, point: Self, rad: f32) void {
             const cos = @cos(rad);
             const sin = @sin(rad);
@@ -140,6 +158,7 @@ pub fn Vec2(T: type) type {
             self = newp;
         }
 
+        /// returns the dot product of a vector
         pub fn dot(self: Self, p2: Self) f32 {
             return self.x * p2.x + self.y * p2.y;
         }
