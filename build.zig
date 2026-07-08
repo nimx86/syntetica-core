da0ebddd1038b93ae1866f83dea6e56b68c3c7ff
Vasilix <lvasilicc@proton.me>
2026-07-08 21:18:19 +0200

    build default vertex and fragment shaders for vulkan

--- build.zig
+++ build.zig
@@ -33,6 +33,10 @@ const Library = struct {
         self.modules.append(self.allocator, .{ .mod = mod, .name = name }) catch @panic("");
     }
 
+    pub fn getModule(self: *Library, id: usize) *std.Build.Module {
+        return self.modules.items[id].mod;
+    }
+
     pub fn confirm(self: *Library) void {
         for(self.modules.items) |mod| {
             for (self.modules.items) |mod1| {
@@ -50,25 +54,25 @@ pub fn build(b: *std.Build) void {
         .b = b,
         .optimize = optimize,
         .target = target,
-        .core = b.addModule("syntetica_core", .{
+        .core = b.addModule("syntetica", .{
             .root_source_file = b.path("src/root.zig"),
             .optimize = optimize,
             .target = target,
         }),
     };
-    syntetica_core.init(b.allocator);
+    syntetica_core.init(b.allocator); // TODO: rewrite this to use more of a tree structure
     _ = syntetica_core.addModule("default");
     _ = syntetica_core.addModule("config");
     _ = syntetica_core.addModule("ui");
     _ = syntetica_core.addModule("ecs");
     _ = syntetica_core.addModule("fs");
-    _ = syntetica_core.addModule("graphics");
+    const mod_graphics = syntetica_core.addModule("graphics");
 
     const vulkan_headers = b.dependency("vulkan_headers", .{});
     const vulkan = b.dependency("vulkan", .{
         .registry = vulkan_headers.path("registry/vk.xml"),
     }).module("vulkan-zig");
-    syntetica_core.core.addImport("vulkan", vulkan);
+    mod_graphics.addImport("vulkan", vulkan);
 
     const zglfw = b.dependency("zglfw", .{
         .target = target,
@@ -78,11 +82,53 @@ pub fn build(b: *std.Build) void {
 
     const zglfw_mod = zglfw.module("root");
     zglfw_mod.addImport("vulkan", vulkan);
-    syntetica_core.core.addImport("zglfw", zglfw_mod);
+    mod_graphics.addImport("glfw", zglfw_mod);
 
     if (target.result.os.tag != .emscripten) {
-        syntetica_core.core.linkLibrary(zglfw.artifact("glfw"));
+        mod_graphics.linkLibrary(zglfw.artifact("glfw"));
     }
+
+    // add shaders
+    const spirv_target = b.resolveTargetQuery(.{
+        .cpu_arch = .spirv32,
+        .os_tag = .vulkan,
+        .cpu_model = .{ .explicit = &std.Target.spirv.cpu.vulkan_v1_2 },
+        .ofmt = .spirv,
+    });
+
+    const vert_spv = b.addObject(.{
+        .name = "default_vert",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/graphics/backend/vulkan/shaders/default_vert.zig"),
+            .target = spirv_target,
+        }),
+        .use_llvm = false,
+    });
+    mod_graphics.addAnonymousImport("default_vertex_shader", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = vert_spv.getEmittedBin(),
+    });
+    // const vert_install = b.addInstallFileWithDir(vert_spv.getEmittedBin(), .prefix, "shaders/vert.spv");
+    // vert_install.step.dependOn(&vert_spv.step);
+    // b.getInstallStep().dependOn(&vert_install.step);
+
+    const frag_spv = b.addObject(.{
+        .name = "default_frag",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/graphics/backend/vulkan/shaders/default_frag.zig"),
+            .target = spirv_target,
+        }),
+        .use_llvm = false,
+    });
+    mod_graphics.addAnonymousImport("default_fragment_shader", .{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = frag_spv.getEmittedBin(),
+    });
+    // const frag_install = b.addInstallFileWithDir(frag_spv.getEmittedBin(), .prefix, "shaders/frag.spv");
+    // frag_install.step.dependOn(&frag_spv.step);
+    // b.getInstallStep().dependOn(&frag_install.step);
 
     // RAYLIB /////////
     const raylib_dep = b.dependency("raylib_zig", .{
@@ -105,6 +151,7 @@ pub fn build(b: *std.Build) void {
         "graphics"
     };
     for (examples) |example_name| {
+        const default_install_path = b.fmt("zig-out/bin/{s}", .{example_name});
         const example_path = b.fmt("examples/{s}", .{example_name}); 
         const example = b.addExecutable(.{
             .name = example_name,
@@ -116,19 +163,17 @@ pub fn build(b: *std.Build) void {
         });
         example.root_module.addImport("syntetica", syntetica_core.core);
 
-        const inst_art = b.addInstallArtifact(example, .{.dest_dir = .{ .override = .bin}});
         const inst_dir = b.addInstallDirectory(.{ 
             .source_dir = b.path(b.fmt("{s}/res", .{example_path})), 
             .install_dir = .bin, 
             .install_subdir = "res" 
         });
-
-        const default_path = b.fmt("zig-out/bin/{s}", .{example_name});
+        const inst_art = b.addInstallArtifact(example, .{.dest_dir = .{ .override = .bin}});
 
         const run_example = b.addSystemCommand(&.{
             b.fmt("{s}/../{s}", .{
                 b.install_path, 
-                example.installed_path orelse default_path
+                example.installed_path orelse default_install_path
             })
         });
         run_example.step.dependOn(&inst_art.step);
@@ -141,4 +186,9 @@ pub fn build(b: *std.Build) void {
         example_step.dependOn(&example.step);
         example_step.dependOn(&run_example.step);
     }
+}
+
+pub fn addShader(b: *std.Build) void {
+    _ = b;
+    @panic("not implemented");
 }
